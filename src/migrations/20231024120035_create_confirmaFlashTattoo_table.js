ee0310eba3937627c155e2641a0dd5cbf02ad8b7
Bieldeid <gabrieldeid.android@gmail.com>
2023-10-24 10:43:04 -0300

    Flash Tatto BD adicionado

--- src/migrations/20231024120035_create_confirmaFlashTattoo_table.js
+++ src/migrations/20231024120035_create_confirmaFlashTattoo_table.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('flashTattoo', function(table){
+  return knex.schema.createTable('confirmaFlashTattoo', function(table){
     table.increments('id').primary();
     table.integer('idAdmin').unsigned().notNullable();
     table.string('nome');
@@ -11,11 +11,9 @@ exports.up = function(knex) {
     table.double('valor2');
     table.double('valor3');
     table.string('imagem');
-    
-    table.foreign('idAdmin').references('id').inTable('admin');
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('flashTattoo');
+  return knex.schema.dropTable('confirmaFlashTattoo');
 };
