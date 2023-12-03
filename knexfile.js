520fbf035699a96b6d209bfc89dae7676b518991
PriscilaDias2003 <priscilagabriellediasribeiro@gmail.com>
2023-12-03 20:07:16 -0300

    Knexfile consertado para subir banco de dados com knex no Railway

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,27 @@
 // knexfile.js
 module.exports = {
     development: {
-    client: 'mysql', 
+    client: 'mysql2', 
     connection: {
-      host: 'localhost', 
+      host: 'roundhouse.proxy.rlwy.net', 
       user: 'root',
-      password: '',
-      database: 'think'
+      password: '5dcd3GgGbGA1eB1-2-f52-1dd2BFDBa2',
+      database: 'railway',
+      port: 28392,
     },
       migrations: {
         directory: './src/migrations'
       },
-      useNullAsDefault: true
+      useNullAsDefault: true,
+      authentication: {
+        authSwitchHandler: ({ pluginName, pluginData, connection }, cb) => {
+          if (pluginName === 'caching_sha2_password') {
+            // Se o método de autenticação for caching_sha2_password, use mysql_native_password
+            return cb(null, Buffer.from([1]));
+          }
+          return cb(new Error('Unsupported auth plugin'));
+        },
+      },
     }
   };
   
