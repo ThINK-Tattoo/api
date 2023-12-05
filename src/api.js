ea0a8d68c97cf86c1b501a501496bbaaa0fd9a4f
Bieldeid <gabrieldeid.android@gmail.com>
2023-12-05 08:34:15 -0300

    Configurando o CORS novamente!

--- src/api.js
+++ src/api.js
@@ -16,11 +16,23 @@ const fecharAgenda = require('./router');
 app.use(express.json()); 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'https://think-tatto.netlify.app');
-  res.setHeader('Access-Control-Allow-Origin', 'https://api-think-tatto.netlify.app');
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://think-tatto.netlify.app',
+    'https://api-think-tatto.netlify.app'
+  ];
+
+  const origin = req.headers.origin;
+
+  if(allowedOrigins.includes(origin)){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  
   next();
 });
 app.use('/src/temp', express.static('src/temp'));
