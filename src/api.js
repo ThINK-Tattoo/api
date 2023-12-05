853b6957c64357b418008a735135bb9942e512a2
Bieldeid <gabrieldeid.android@gmail.com>
2023-12-05 08:22:24 -0300

    Dando update no CORS

--- src/api.js
+++ src/api.js
@@ -17,6 +17,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://think-tatto.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://api-think-tatto.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
